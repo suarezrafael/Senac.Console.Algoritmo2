621d60d5364d67499ea45e84bbaab17b8774cdec
Rafael Suarez <rafaelv_s@hotmail.com>
2025-03-25 20:36:30 -0300

    CRIACAO DAS VARIAVEIS

--- Algoritmo2/Algoritmo2/Program.cs
+++ Algoritmo2/Algoritmo2/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int idade = 10;
+string nomeCliente = "Rafael Suarez";
+decimal preco = 20M;
+char letra = 'a';
+var nomeCompleto = "Rafael Vieira Suarez";
+var altura = 180;
+
+
