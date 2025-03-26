04f237c3d884feaca15a9ced705ef2419be2f5b0
Rafael Suarez <rafaelv_s@hotmail.com>
2025-03-25 21:50:18 -0300

    LEITURA DE VARIAVEIS

--- Algoritmo2/Algoritmo2/Program.cs
+++ Algoritmo2/Algoritmo2/Program.cs
@@ -5,5 +5,19 @@ char letra = 'a';
 var nomeCompleto = "Rafael Vieira Suarez";
 var altura = 180;
 
+Console.Write("Digite seu nome: ");
+nomeCompleto = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("Bem vindo " + nomeCompleto);
+Console.WriteLine();
+Console.Write("Digite sua idade: ");
+var linha = Console.ReadLine();
+idade = int.Parse(linha);
+Console.WriteLine("Idade informada " + idade);
+if(idade < 18)
+    Console.WriteLine("Você não pode estar aqui!");
+
+Console.ReadKey();
+
 
 
